5c5e1f649ae5fb225b3aa11f811a059ef3eac2b4
ekoow <deckpich.zip@orange.fr>
2024-08-18 11:32:02 +0200

    Fix task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """1-concurrent_coroutines.py"""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
         Run `wait_random` `n` times concurrently, with a maximum delay of `max_delay`.
     """
