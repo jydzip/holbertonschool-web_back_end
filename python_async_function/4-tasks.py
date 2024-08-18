ab305358a8b9ec0875935701ddd6636913d4ac61
ekoow <deckpich.zip@orange.fr>
2024-08-18 11:36:15 +0200

    Fix task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -11,7 +11,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-        Run `task_wait_random` `n` times concurrently, with a maximum delay of `max_delay`.
+        Run `task_wait_random` `n` times concurrently.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
