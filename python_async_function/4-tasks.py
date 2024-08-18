8105a39ca32ca88b899f746af8a8f3d66838682b
ekoow <deckpich.zip@orange.fr>
2024-08-18 11:35:36 +0200

    Fix task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    4-tasks.py
+"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+        Run `task_wait_random` `n` times concurrently, with a maximum delay of `max_delay`.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    
+    return delays
