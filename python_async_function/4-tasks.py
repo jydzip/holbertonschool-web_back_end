370f041789fad6a1e611c3839b63a354414e1b5a
jydzip <dckjeremy.zip@gmail.com>
2024-12-05 14:11:55 +0100

    Fix

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -15,9 +15,9 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
-    
+
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
-    
+
     return delays
