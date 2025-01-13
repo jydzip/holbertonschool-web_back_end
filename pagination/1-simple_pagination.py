29011a18d0fe3e58f599fe6cf01de948c25f8ec2
jydzip <dckjeremy.zip@gmail.com>
2025-01-13 13:31:52 +0100

    Add task 2

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -3,8 +3,12 @@
 
 import csv
 from typing import List
+from typing import Tuple
 
-index_range = __import__('0-simple_helper_function').index_range
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple of size two containing a start index and an end index"""
+    return ((page - 1) * page_size, page * page_size)
 
 
 class Server:
