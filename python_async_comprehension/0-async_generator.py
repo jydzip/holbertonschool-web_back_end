e5139ea05afbee567c35dbd7ee3d3b2cb652a8f8
jydzip <dckjeremy.zip@gmail.com>
2025-01-13 10:45:49 +0100

    Add task 1

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,8 @@
+async def async_generator():
+    """Yield a random number between 0 and 10 every 1 second."""
+    import asyncio
+    import random
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
