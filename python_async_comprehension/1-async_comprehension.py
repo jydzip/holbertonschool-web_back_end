2ffedd34d471e1f35e935e4b9fb23466e98867b7
jydzip <dckjeremy.zip@gmail.com>
2025-01-13 12:41:25 +0100

    Add task

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Async Generator"""
 
+async_generator = __import__('0-async_generator').async_generator
+
 async def async_comprehension() -> list[float]:
     """Return 10 random numbers between 0 and 10."""
-    import random
-
-    return [random.uniform(0, 10) async for _ in range(10)]
+    return await async_generator()
