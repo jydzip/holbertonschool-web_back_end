fc2826a1543021a2c3cde597c90eda59d2628fc0
jydzip <dckjeremy.zip@gmail.com>
2025-01-21 00:27:43 +0100

    Fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,12 +2,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.exit();
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
 
-process.on('SIGINT', () => {
-  process.stdout.write('\nThis important software is now closing\n');
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
