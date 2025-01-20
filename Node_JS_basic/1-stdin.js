7218e32ef82896f66e55a1dac25855c8cc83a363
jydzip <dckjeremy.zip@gmail.com>
2025-01-20 19:07:37 +0100

    Fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,15 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+// Listen for user input
+process.stdin.on('data', (data) => {
+    const name = data.toString().trim(); // Convert input to string and trim whitespace
+    console.log(`Your name is: ${name}`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit(); // Exit the process after displaying the closing message
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
-});
-
-rl.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
+// Handle program exit gracefully
+process.on('SIGINT', () => {
+    process.stdout.write('\nThis important software is now closing\n');
+    process.exit();
 });
--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-
 function countStudents(path) {
     try {
         const data = fs.readFileSync(path, 'utf8');
@@ -32,5 +31,4 @@ function countStudents(path) {
         throw new Error('Cannot load the database');
     }
 }
-
 module.exports = countStudents;
