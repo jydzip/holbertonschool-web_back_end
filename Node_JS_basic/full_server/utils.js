f37092112ee4b239dc54261f7bd201b11b86775f
jydzip <dckjeremy.zip@gmail.com>
2025-01-20 20:33:09 +0100

    Add

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function readDatabase(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if (err) {
+                reject(new Error('Cannot load the database'));
+                return;
+            }
+
+            const fields = {};
+            const students = data.split('\n').filter((student) => student.trim() !== '');
+            if (students.length === 0) {
+                reject(new Error('Cannot load the database'));
+                return;
+            }
+
+            students.shift();
+
+            for (const student of students) {
+                const cols = student.split(',');
+                const field = cols[3];
+                if (!fields[field]) {
+                    fields[field] = [];
+                }
+                fields[field].push(cols[0]);
+            }
+
+            resolve(fields);
+        });
+    });
+}
+
+module.exports = readDatabase;
