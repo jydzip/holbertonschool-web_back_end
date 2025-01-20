ec3de45230b03c034b3a5224db26c4e317ff1ef7
jydzip <dckjeremy.zip@gmail.com>
2025-01-20 21:16:50 +0100

    add

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,8 +5,10 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+  process.exit();
 });
 
-process.stdin.on('end', () => {
+process.on('SIGINT', function() {
   console.log('This important software is now closing');
+  process.exit();
 });
