5eb2848658f1317944d2de47c9a084a19438d99f
jydzip <dckjeremy.zip@gmail.com>
2025-01-21 00:21:35 +0100

    Fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,6 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
   process.exit();
 });
 
