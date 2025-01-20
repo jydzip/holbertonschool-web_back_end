e9d2bfecde4413efe9346d7e484277d83d2e1c4c
jydzip <dckjeremy.zip@gmail.com>
2025-01-20 18:44:50 +0100

    fIX3

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -12,4 +12,5 @@ rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
 
 rl.on('close', () => {
   console.log('This important software is now closing');
+  process.exit(0);
 });
