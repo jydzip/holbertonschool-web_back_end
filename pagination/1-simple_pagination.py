70cf5e4b64dd7b65b695b13ed3d896f557594c2f
jydzip <dckjeremy.zip@gmail.com>
2025-01-20 20:38:59 +0100

    Add

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -35,6 +35,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Return the appropriate page of the dataset
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
