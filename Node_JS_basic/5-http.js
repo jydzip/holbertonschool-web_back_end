4a69327e47619d9c041c2c4682231693dc3d15c1
jydzip <dckjeremy.zip@gmail.com>
2025-01-21 00:38:49 +0100

    Fix

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,6 +1,41 @@
 // 5-http.js
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const students = data.split('\n').filter((student) => student.trim() !== '');
+      if (students.length === 0) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      students.shift();
+
+      let response = `Number of students: ${students.length}\n`;
+
+      const fields = {};
+      students.forEach((student) => {
+        const studentInfo = student.split(',');
+        if (!fields[studentInfo[3]]) {
+          fields[studentInfo[3]] = [];
+        }
+        fields[studentInfo[3]].push(studentInfo[0]);
+      });
+
+      for (const i of Object.keys(fields)) {
+        response += `Number of students in ${i}: ${fields[i].length}. List: ${fields[i].join(', ')}\n`;
+      }
+      resolve(response.trim());
+    });
+  });
+}
 
 const port = 1245;
 
@@ -12,15 +47,16 @@ const app = http.createServer((req, res) => {
   } else if (req.url === '/students') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.write('This is the list of our students\n');
 
-    countStudents(process.argv[2])
-      .then((data) => {
-        res.end(data);
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
+    try {
+      countStudents(process.argv[2])
+        .then((response) => {
+          res.end(`This is the list of our students\n${response}`);
+        });
+    } catch (error) {
+      res.statusCode = 500;
+      res.end(error.message);
+    }
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
