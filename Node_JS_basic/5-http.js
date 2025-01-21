fbb49565f65afa674fe4c3851440b1996882d4c5
jydzip <dckjeremy.zip@gmail.com>
2025-01-21 01:04:37 +0100

    Fix async

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,40 +1,36 @@
 // 5-http.js
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+const countStudents = async (path) => {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1).map((line) => line.split(',')).filter((student) => student.length === 4);
+
+    let response = `Number of students: ${students.length}\n`;
+
+    const fields = {};
+    students.forEach((student) => {
+      if (!fields[student[3]]) {
+        fields[student[3]] = [];
       }
-
-      const students = data.split('\n').filter((student) => student.trim() !== '');
-      students.shift();
-
-      let response = `Number of students: ${students.length}\n`;
-
-      const fields = {};
-      students.forEach((student) => {
-        const studentInfo = student.split(',');
-        if (!fields[studentInfo[3]]) {
-          fields[studentInfo[3]] = [];
-        }
-        fields[studentInfo[3]].push(studentInfo[0]);
-      });
-
-      for (const i of Object.keys(fields)) {
-        response += `Number of students in ${i}: ${fields[i].length}. List: ${fields[i].join(', ')}\n`;
-      }
-      resolve(response.trim());
+      fields[student[3]].push(student[0]);
     });
-  });
-}
+
+    for (const i of Object.keys(fields)) {
+      response += `Number of students in ${i}: ${fields[i].length}. List: ${fields[i].join(', ')}\n`;
+    }
+
+    return response.trim();
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
 
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -44,14 +40,8 @@ const app = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
 
     try {
-      countStudents(process.argv[2])
-        .then((response) => {
-          res.end(`This is the list of our students\n${response}`);
-        })
-        .catch((error) => {
-          res.statusCode = 500;
-          res.end(error.message);
-        });
+      const response = await countStudents(process.argv[2]);
+      res.end(`This is the list of our students\n${response}`);
     } catch (error) {
       res.statusCode = 500;
       res.end(error.message);
@@ -59,7 +49,7 @@ const app = http.createServer((req, res) => {
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Not found.');
+    res.end('Not found');
   }
 });
 
