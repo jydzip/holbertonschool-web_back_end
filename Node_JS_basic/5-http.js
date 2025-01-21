d4dc3050e6382b058c4f9e52847ba38fe7ec4b70
jydzip <dckjeremy.zip@gmail.com>
2025-01-21 01:19:18 +0100

    Fix

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,42 +1,49 @@
 // 5-http.js
 const http = require('http');
-const fs = require('fs').promises;
+const fs = require('fs');
 
-const databasePath = process.argv[2];
-if (!databasePath) {
-  console.error('Usage: node 5-http.js <database.csv>');
-  process.exit(1);
-}
-
-const countStudents = async (path) => {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const students = lines.slice(1).map((line) => line.split(',')).filter((student) => student.length === 4);
-
-    let response = `Number of students: ${students.length}\n`;
-
-    const fields = {};
-    students.forEach((student) => {
-      if (!fields[student[3]]) {
-        fields[student[3]] = [];
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
-      fields[student[3]].push(student[0]);
+
+      const students = data.split('\n').filter((student) => student.trim() !== '');
+      if (students.length === 0) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      students.shift();
+
+      let response = `Number of students: ${students.length}`;
+      console.log(response);
+      response += '\n';
+
+      const fields = {};
+      students.forEach((student) => {
+        const studentInfo = student.split(',');
+        if (!fields[studentInfo[3]]) {
+          fields[studentInfo[3]] = [];
+        }
+        fields[studentInfo[3]].push(studentInfo[0]);
+      });
+
+      for (const i of Object.keys(fields)) {
+        const responseField = `Number of students in ${i}: ${fields[i].length}. List: ${fields[i].join(', ')}`;
+        response += `${responseField}\n`;
+        console.log(responseField);
+      }
+      resolve(response);
     });
-
-    for (const i of Object.keys(fields)) {
-      response += `Number of students in ${i}: ${fields[i].length}. List: ${fields[i].join(', ')}\n`;
-    }
-
-    return response.trim();
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-};
+  });
+}
 
 const port = 1245;
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -46,8 +53,14 @@ const app = http.createServer(async (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
 
     try {
-      const response = await countStudents(databasePath);
-      res.end(`This is the list of our students\n${response}`);
+      countStudents(process.argv[2])
+        .then((response) => {
+          res.end(`This is the list of our students\n${response}`);
+        })
+        .catch((error) => {
+          res.statusCode = 500;
+          res.end(error.message);
+        });
     } catch (error) {
       res.statusCode = 500;
       res.end(error.message);
@@ -55,7 +68,7 @@ const app = http.createServer(async (req, res) => {
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Not found');
+    res.end('Not found.');
   }
 });
 
